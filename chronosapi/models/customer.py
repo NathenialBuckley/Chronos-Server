140ec0af01bdada937cb6c72e17b0f03026bd007
Nathenial Buckley <nathenialbuckley98@gmail.com>
2022-12-09 10:26:42 -0600

    Added client authentication

--- chronosapi/models/customer.py
+++ chronosapi/models/customer.py
@@ -6,6 +6,6 @@ class Customer(models.Model):
     address = models.CharField(max_length=155)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
-    postalCode = models.IntegerField(max_length=5)
-    phone = models.IntegerField(max_length=11)
-    userId = models.ForeignKey("User", on_delete=models.CASCADE)
+    postalCode = models.IntegerField
+    phone = models.IntegerField
+    userId = models.OneToOneField(User, on_delete=models.CASCADE)
