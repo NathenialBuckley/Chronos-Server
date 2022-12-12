f31c20054829a86ea3985ccb0d4a6801e6a712d6
Nathenial Buckley <nathenialbuckley98@gmail.com>
2022-12-12 11:38:40 -0600

    Added urls

--- chronosapi/views/__init__.py
+++ chronosapi/views/__init__.py
@@ -2,5 +2,5 @@ from .auth import login_user, register_user
 from .customerview import CustomerView
 from .reviewview import ReviewView
 from .watchview import WatchView
-from .favoritewatchview import FavoriteWatch
-from .watchtypeview import WatchType
+from .favoritewatchview import FavoriteWatchView
+from .watchtypeview import WatchTypeView
--- chronosserver/urls.py
+++ chronosserver/urls.py
@@ -2,9 +2,23 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from chronosapi.views import register_user, login_user
+from rest_framework import routers
+from chronosapi.views import CustomerView
+from chronosapi.views import FavoriteWatchView
+from chronosapi.views import ReviewView
+from chronosapi.views import WatchTypeView
+from chronosapi.views import WatchView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'customers', CustomerView, 'customer')
+router.register(r'favoritewatches', FavoriteWatchView, 'favoritewatch')
+router.register(r'reviews', ReviewView, 'review')
+router.register(r'watchtypes', WatchTypeView, 'watchtype')
+router.register(r'watches', WatchView, 'watch')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
