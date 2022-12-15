8cbb3e165edcc87f787e4d038b11160bf38c27f9
Nathenial Buckley <nathenialbuckley98@gmail.com>
2022-12-15 15:24:36 -0600

    Updated Models

--- chronosapi/models/watch.py
+++ chronosapi/models/watch.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Watch(models.Model):
     name = models.CharField(max_length=50)
-    style = models.CharField(max_length=50)
+    watchtype = models.ForeignKey(
+        "WatchType", on_delete=models.CASCADE, related_name="watches")
     price = models.CharField(max_length=50)
     customer = models.ForeignKey("Customer", on_delete=models.CASCADE)
--- chronosapi/models/watchtype.py
+++ chronosapi/models/watchtype.py
@@ -3,4 +3,3 @@ from django.db import models
 
 class WatchType(models.Model):
     type = models.CharField(max_length=50)
-    watch = models.ForeignKey("Watch", on_delete=models.CASCADE)
